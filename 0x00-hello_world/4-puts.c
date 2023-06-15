c345b7d6100048316155ffdad8b4c6442b8e6e35
souha <souhakhalfaoui4@gmail.com>
2023-06-15 10:36:02 -0700

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * -main
+ *
+ *  prints with fonction puts
+ *
+ *  return: à (success(
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
