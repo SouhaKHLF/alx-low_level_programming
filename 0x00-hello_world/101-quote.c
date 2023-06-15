f549d7c41159ff9197ab6aa41604ccd44dc12fc7
souha <souhakhalfaoui4@gmail.com>
2023-06-15 12:45:04 -0700

    quote commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using function write
+ *
+ * Return: 1 (not success)
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	whrite(1, quo, 59);
+return (1);
+}
