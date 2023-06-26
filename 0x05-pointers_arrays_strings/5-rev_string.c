9afb1638ac5ebe4d0d60594e570296a22d845d45
souha <souhakhalfaoui4@gmail.com>
2023-06-26 10:26:19 -0700

    commit pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *rev_string - function that reverses a string
+ *
+ *@s: string parameter input
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	/*find string length without null char*/
+	for (l = 0; s[1] != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because array start from 0*/
+		s[l - 1 - i] = temp;
+	}
+}
+
