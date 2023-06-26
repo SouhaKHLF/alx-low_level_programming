8084a1868417150b52e39b6de929024a87ef72ef
souha <souhakhalfaoui4@gmail.com>
2023-06-26 09:42:18 -0700

    commit pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string input parameter
+ *
+ * Return: length of a string
+ */
+
+void _strlen(char *s)
+{
+	int cnt;
+
+	for (cnt = 0; *s != '\0'; s++)
+		ctr++;
+	return (ctr);
+}
